1f9a89ac6d48b48a0a93a5cb19b9999888e914f7
ZhiFeng Hu <hufeng1987@gmail.com>
2015-03-28 08:18:52 +0800

    Fixed connect problem

--- tcpheartping.go
+++ tcpheartping.go
@@ -12,14 +12,18 @@ func main() {
     for longpoll == 1  {
         conn, err := net.Dial("tcp", "127.0.0.1:9070")
         if err != nil {
+            fmt.Println("Service not available")
+            time.Sleep(10*time.Second)
+            continue
+        } else {
+            fmt.Fprintf(conn, "GET /\r\n\r\n")
+            status, err := bufio.NewReader(conn).ReadString('\n')
+            fmt.Println(status)
+            if err != nil {
+                fmt.Println(err)
+            }
+            fmt.Println("ok")
         }
-        fmt.Fprintf(conn, "GET /\r\n\r\n")
-        status, err := bufio.NewReader(conn).ReadString('\n')
-        fmt.Println(status)
-        if err != nil {
-            fmt.Println(err)
-        }
-        fmt.Println("ok")
         time.Sleep(10*time.Second)
     }
 }
