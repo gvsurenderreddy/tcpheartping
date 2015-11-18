f0e6ef444f9aa2ff1cf3ba6b9b5bc1ac0d66e29d
ZhiFeng Hu <hufeng1987@gmail.com>
2015-11-18 09:19:45 +0800

    Update tcp time out
    Signed-off-by: ZhiFeng Hu <hufeng1987@gmail.com>

--- tcpheartping.go
+++ tcpheartping.go
@@ -26,7 +26,7 @@ func main() {
 			fmt.Println("Service not available, Try to restart ssh service")
 			restartService()
 		} else {
-			conn.SetDeadline(time.Now().Add(time.Second * 9))
+			_ = conn.SetDeadline(time.Now().Add(time.Second * 15)) //Set dead line for 15 seconds
 			fmt.Fprintf(conn, "GET /\r\n\r\n")
 			status, err := bufio.NewReader(conn).ReadString('\n')
 			fmt.Println(status)
